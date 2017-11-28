3f030ef5b7c6cae40ecb15de95dc1878519a755e
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-11-28 12:04:35 -0600

    initiated menu routes registration in main file

--- wp-rest-routes.php
+++ wp-rest-routes.php
@@ -15,4 +15,23 @@
  * License: MIT
  */
 
- 
+ if ( ! defined( 'ABSPATH' ) ) {
+     exit; //Exits if accessed directly
+ }
+
+ include_once 'includes/menus.php';
+
+ if ( ! function_exists ( 'wp_rest_routes_init' ) ) :
+    /**
+     * Init WP REST ROUTES
+     * 
+     * @since 1.0
+     */
+    function wp_rest_routes_init() {
+        $menu_class = new Menu_Routes();
+        add_filter( 'rest_api_init', array( $menu_class, 'register_routes' ) );
+    }
+
+    add_action( 'init', 'wp_rest_routes_init');
+
+endif;
