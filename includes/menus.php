fa50b4a9f6f6ef2f865943b273559c15d4b3d86f
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-12-04 19:46:13 -0600

    menus sending correctly

--- includes/menus.php
+++ includes/menus.php
@@ -63,6 +63,13 @@ if ( ! class_exists( 'Menu_Routes' ) ) :
                     'callback' => array( $this, 'get_menu' )
                 )
                 ));
+
+            register_rest_route( self::get_plugin_namespace(), '/menus/all', array(
+                array(
+                    'methods' => 'GET',
+                    'callback' => array( $this, 'get_all_menus' )
+                )
+                ));
         }
 
         /**
@@ -73,7 +80,7 @@ if ( ! class_exists( 'Menu_Routes' ) ) :
          * 
          * the original plugin sanitized the data, and made it look all pretty.  Maybe for v2
          */
-        public static function get_menus() {
+        public function get_menus() {
             $wp_menus = wp_get_nav_menus();
 
             return $wp_menus;
@@ -92,6 +99,40 @@ if ( ! class_exists( 'Menu_Routes' ) ) :
             $wp_menu_items = wp_get_nav_menu_items( $id );
 
             return $wp_menu_items;
+        }
+
+        /**
+         * Get All Menus
+         * 
+         * @since 1.0.1
+         * @return array of all menus
+         * 
+         */
+        public function get_all_menus () {
+            $list_of_menus = self::get_menus();
+
+            $all_menus = array();
+            
+            foreach ($list_of_menus as $menu) {
+                $single_menu = (array) $menu;
+
+                $term_id = $single_menu['term_id'];
+
+                $current_menu = wp_get_nav_menu_items( $term_id );
+
+                $new_menu = (array) $current_menu;
+
+                $sanitized_menu = array(
+                    'name' => $single_menu['name'],
+                    'slug' => $single_menu['slug'],
+                    'items' => $new_menu
+                );
+
+                $all_menus[] = $sanitized_menu;
+
+            }
+
+            return $all_menus;
         }
 
         //future features: add support for nested menu items and menu location
